e3520b3e2cfc1c682ad0f6bcfbcdc9296c4fd2ab
logiteca7 <aaaa777@users.noreply.github.com>
2020-08-28 02:32:35 +0900

    parser is once completed

--- lib/url_parser.rb
+++ lib/url_parser.rb
@@ -1,14 +1,20 @@
+require 'digest'
+
 module YoutubeDL
 
   module URL
-    #extend URI
+    # init constants
+    YOUTUBE_IDENTIFIER = :youtube.freeze
+    NICONICO_IDENTIFITER = :niconico.freeze
+    RAWDATA_IDENTIFIER = :url.freeze
 
     # URL parser of YoutubeDL
-    def self.parse(original_uri)
+    def self.parse(original_url)
       # call URI.parse
-      uri = URI.parse(original_uri)
-      
+      uri = URI.parse(original_url)
+
       case uri.hostname
+
       # in case of youtube, uri will be exchanged into https://youtu.be/#{id}
       when '(www.)?youtube.com'
         # in case of https://www.youtube.com/watch?v=#{id}
@@ -17,29 +23,74 @@ module YoutubeDL
         id = query['v']
 
         uri = URI.parse(youtube_url(id))
-        uri = URL::Generic.new(*parse_arr(uri))
-        #service = :youtube
-        #uri = super(youtube_url(@id))
+        uri = URL::Generic.new(*parse_arr(uri), original_url: original_url, service: YOUTUBE_IDENTIFIER)
+
+
       when 'youtu.be'
         # in case of https://youtu.be/#{id}
         id = uri.path.chomp('/').delete_prefix('/')
 
         uri = URI.parse(youtube_url(id))
-        uri = URL::Generic.new(*parse_arr(uri))
-        #service = :youtube
+        uri = URL::Generic.new(*parse_arr(uri), id: id, original_url: original_url, service: YOUTUBE_IDENTIFIER)
+
+
       when /(www.)?nicovideo\.jp/, 'nico.ms'
         # nico.ms and www.nicovideo.jp redirects www.nicovideo.jp
-        id = uri.path.chomp('/').split('/').last[2..-1]#.delete_prefix('sm')
+        id = uri.path.chomp('/').split('/').last[2..-1]
 
-        uri = URI.parse(niconico_url('sm' + id))
-        #p parse_hash(uri)
-        uri = URL::Generic.new(*parse_arr(uri))
-        service = :nicovideo
+        uri = URI.parse(niconico_url(id))
+        uri = URL::Generic.new(*parse_arr(uri), id: id, original_url: original_url, service: NICONICO_IDENTIFITER)
+
+
       else
-        #@id = nil
-        service = :url
+        # any audio file resources of url
+        id = Digest::MD5.hexdigest(original_url)
+
+        uri = URL::Generic.new(*parse_arr(uri), id: id, original_url: original_url, service: RAWDATA_IDENTIFIER)
+
       end
 
+      uri
+    end
+
+    # avoid overriding build method in URI class
+    def self.build_url(service, id)
+      case service
+      when YOUTUBE_IDENTIFIER
+        uri = parse(youtube_url(id))
+      when NICONICO_IDENTIFITER
+        uri = parse(niconico_url(id))
+      when RAWDATA_IDENTIFIER
+        # id should be url
+        uri = parse(id)
+      else
+        raise 'unknown service was specified'
+      end
+      uri
+    end
+
+
+    # restore original url data
+    # returns nil if restore failed
+    def self.build_from_filename(filename, original_url = nil)
+      md = filename.match(/([a-zA-Z0-9]+)-(.+)\.(.+)/)
+      raise 'not matched' unless md
+      service, id, ext = md.captures
+      service = service.to_sym
+
+      case service
+      when YOUTUBE_IDENTIFIER
+        uri = parse(youtube_url(id))
+      when NICONICO_IDENTIFITER
+        uri = parse(niconico_url(id))
+      when RAWDATA_IDENTIFIER
+        # id is hash value of original url string
+        return nil unless id == Digest::MD5.hexdigest(original_url)
+        uri = parse(original_url)
+      else
+        return nil
+        #raise 'unknown service was specified'
+      end
       uri
     end
 
@@ -47,72 +98,50 @@ module YoutubeDL
 
     # for backward compatibility
     def self.parse_arr(uri)
-      [uri.scheme, uri.userinfo, uri.host, uri.port, uri.registry, uri.path, uri.opaque, uri.query, uri.fragment]
+      # check whether url using default port
+      port = {'http' => 80, 'https' => 443}.find{|sc, port| uri.scheme == sc && uri.port == port} ? nil : uri.port
+      [uri.scheme, uri.userinfo, uri.host, port, uri.registry, uri.path, uri.opaque, uri.query, uri.fragment]
     end
 
     # deprecated
     def self.parse_hash(uri)
-      {scheme: uri.scheme, userinfo: uri.userinfo, host: uri.host, port: uri.port, registry: uri.registry, path: uri.path, opaque: uri.opaque, query: uri.query, fragment: uri.fragment}
+      {scheme: uri.scheme, userinfo: uri.userinfo, host: uri.host, port: nil, registry: uri.registry, path: uri.path, opaque: uri.opaque, query: uri.query, fragment: uri.fragment}
     end
 
     # these method works removing unrelated query for downloading
     def self.niconico_url(id)
-      "https://nico.ms/#{id}"
+      "https://nico.ms/sm#{id}"
     end
-    
+
     def self.youtube_url(id)
       "https://youtu.be/#{id}"
     end
-      
+
 
     # URL instance contains video service data
     class Generic < URI::Generic
-      
-      def initialize(*arr, **hash)
-        if hash.empty?
-          super(*arr)
-        else
-          super(**hash)
-        end
-      end
-      
-      # avoid overriding build method in URI class
-      def build_url(service, id)
-        case service
-        when :youtube, :yt
-          uri = parse(youtube_url(id))
-        when :nicovideo, :nico
-          uri = parse(niconico_url(id))
-        else
-          raise 'unknown service was specified'
-        end
-      end
-      
 
-      
-      def service_to_sym(service)
-        case service
-        when 'youtu.be', :youtube, :yt
-          :youtube
-        when 'niconico.jp', :nicovideo, :nico
-          :nicovideo
-        else
-          self.to_s
-        end
+      attr_reader :service
+
+      def initialize(*arr, **hash)
+        #build_elements = [:scheme ,:userinfo, :host, :port, :registry, :path, :opaque, :query, :fragment]
+        #super(hash.select{|k| build_elements.include?(k)})
+        super(*arr)
+
+        # YoutubeDL original param
+        @service, @original_url, @id = hash[:service], hash[:original_url], hash[:id]
       end
-      
-      def to_cache_name
-        
+
+
+      # cache filename => service-id.mp3
+      # ex: niconico-12345.mp3, youtube-ABCDE.mp3, url-#{url-hash}
+      # ~~ filename should not contain '-' ather than separator of service and id ~~
+      def to_filename(ext = 'mp3')
+        "#{@service.to_s}-#{@id.to_s}.#{ext}"
       end
-      
-      def to_download_format
-        service_type = service
-        
-        
-      end
-      
+
     end
+
   end
-  
-  
+
 end
